9927423fa649ecc2d67eada6b10b55bd776724a5
Mudheera Kariyawasam <2020is055@stu.ucsc.cmb.ac.lk>
2022-11-14 11:44:52 +0530

    location changed

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
          //session_register("myemail");
          $_SESSION['login_user'] = $myemail;
          
-         header("location: welcome.php");
+         header("location: additem.php");
       }else {
          $error = "Your Login Name or Password is invalid";
       }
