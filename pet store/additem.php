31c08d55bbaad7ae0737818297f3a88107e345c8
Mudheera Kariyawasam <2020is055@stu.ucsc.cmb.ac.lk>
2022-11-15 14:07:25 +0530

    Made the navigation bar and the header.

--- pet store/additem.php
+++ pet store/additem.php
@@ -1,5 +1,7 @@
 <?php
    include("dbconnection.php");
+   include("header.php");
+   
    session_start();
    
    if($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -29,43 +31,64 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/additem.css">
+    
     <title>Add New Item</title>
 </head>
 <body>
-    <div class="container">
-        <p>
-            <span class="topic">Add New Item</span>
+    <div class="main-container">
+        
+    <div class="navbar">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a class="active" href="#">Pet Items</a></li>
+                <li><a href="#">Medicine</a></li>
+                <li><a href="#">Leave Requests</a></li>
+                <li><a href="#">My Profile</a></li>
+                <li><a href="#">Logout</a></li>
+            </ul>
+        </div>
+        
+
+        
+        <div class="container">
+        <div class="content">
+            <span class="pet-item">PET ITEMS</span>
+            <br>
+            <span class="main-topic">Add New Item</span>
             <span class="sub-topic">Add the information about the item</span>
-        </p>
-        <form action="" method="POST">
-            <label>Item ID</label><br>
-            <input type="text" name="item_id" placeholder="Item ID"><br>
-            <label>Product Name</label><br>
-            <input type="text" name="item_name" placeholder="Product Name"><br>
-            <label>Product Brand</label><br>
-            <input type="text" name="item_brand" placeholder="Product Brand"><br>
-            <label>Qty</label><br>
-            <input type="text" name="item_qty" placeholder="Quantity"><br>
-            <label>Price</label><br>
-            <input type="text" name="item_price" placeholder="Price"><br>
-            <label>Category</label><br>
-            <div class="dropdown-list">
-                <select name="item_category" id="Category" class="category">
-                    <option value="Pet Food">Pet Food</option>
-                    <option value="Sleeping Items">Sleeping Items</option>
-                    <option value="Collars">Collars</option>
-                    <option value="Toys">Toys</option>
-                    <option value="Combs">Toys</option>
-                    <option value="Food Bowls">Food Bowls</option>
-                    <option value="Other">Other</option>
-                </select><br>
-            </div>
-            <p>
-                <button class="btn-add" type="submit">Add </button>
-                <button class="btn-exit"type="submit">Exit</button>
-            </p>
-        </form> 
+            <link rel="stylesheet" href="css/add.css">
+            <br>
+            
+            <form action="" method="POST">
+                <label>Item ID</label><br>
+                <input type="text" name="item_id" placeholder="Item ID"><br>
+                <label>Product Name</label><br>
+                <input type="text" name="item_name" placeholder="Product Name"><br>
+                <label>Product Brand</label><br>
+                <input type="text" name="item_brand" placeholder="Product Brand"><br>
+                <label>Qty</label><br>
+                <input type="text" name="item_qty" placeholder="Quantity"><br>
+                <label>Price</label><br>
+                <input type="text" name="item_price" placeholder="Price"><br>
+                <label>Category</label><br>
+                <div class="dropdown-list">
+                    <select name="item_category" id="Category" class="category">
+                        <option value="Pet Food">Pet Food</option>
+                        <option value="Sleeping Items">Sleeping Items</option>
+                        <option value="Collars">Collars</option>
+                        <option value="Toys">Toys</option>
+                        <option value="Combs">Toys</option>
+                        <option value="Food Bowls">Food Bowls</option>
+                        <option value="Other">Other</option>
+                    </select><br>
+                </div>
+            </form> 
+            <button class="btn-add" type="submit">Add </button>
+            <button class="btn-exit"type="submit">Exit</button>
+        </div>
+    </div>
+
+
     </div>
 </body>
 </html>
