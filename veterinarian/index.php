13380a879afaf3177191b8b5f01e907b4daf396e
senuri <senuridilshara@gmail.com>
2022-12-23 10:12:24 +0530

    integrated the system

---
+++ veterinarian/index.php
@@ -0,0 +1,183 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="./css/landing_page.css">
+<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans&family=Amatic+SC&family=Poppins&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<div class="nav-bar">
+<div class="nav-left"><img class="logo" src="./images/logo_transparent black.png"></div>
+<div class="nav-right">
+<ul class="navbar-ul">
+<li>Home</li>
+<li>About Us</li>
+<li>VIP Programs</li>
+<li>Services</li>
+<li>Pet Store</li>
+<li>Contact Us</li>
+<li>Make an appointment</li>
+<li><a href="controllers/registration.php">Sign Up</a></li>
+<li><a href="controllers/login.php">Login</a></li>
+</ul>
+
+
+
+</div>
+</div>
+<div class="clear"></div>
+
+<div class="home-section">
+
+<br/><br/><br/><br/>
+<div class="welcome"><p><div class="line1"> Welcome To</div><br/> <div class="line2">Pet Life</div></p></div>
+<button>LOGIN</button>
+</div>
+
+<div class="section-2">
+<br/><br/><br/><br/>
+
+<div class="sec-1"><center><div class="include"><img class="include-img" src="./images/landing/Vector.png"><br/><font color="white"></font></div></center>
+
+<br/><center><div class="sec-title">Qualified Doctor</div></center>
+<br/><P>Lorem ipsum dolor sit amet consectetur.<br/>
+ Convallis non ornare amet sed sagittis lobortis at vulputate.</br> 
+Turpis orci penatibus nisl ullamcorper nisl at lectus. </br>
+Fermentum sit tortor platea habitant.</p>
+
+</div>
+
+<div class="sec-2"><center><div class="include"><img class="include-img" src="./images/landing/Vector1.png"><br/><font color="white"></font></div></center>
+
+<br/><center><div class="sec-title">Advanced Technology</div></center>
+<br/><P>Lorem ipsum dolor sit amet consectetur.<br/>
+ Convallis non ornare amet sed sagittis lobortis at vulputate.</br> 
+Turpis orci penatibus nisl ullamcorper nisl at lectus. </br>
+Fermentum sit tortor platea habitant.</p>
+</div>
+
+<div class="sec-3"><center><div class="include"><img class="include-img" src="./images/landing/Vector3.png"><br/><font color="white"></font></div></center>
+
+
+<br/><center><div class="sec-title">Pet Friendly Services</div></center>
+<br/><P>Lorem ipsum dolor sit amet consectetur.<br/>
+ Convallis non ornare amet sed sagittis lobortis at vulputate.</br> 
+Turpis orci penatibus nisl ullamcorper nisl at lectus. </br>
+Fermentum sit tortor platea habitant.</p>
+</div>
+
+</div>
+<div class="clear"></div>
+
+
+<div class="section-3">
+<br/><br/>
+<div class="section-3-title">Welcome to Pet Life</div>
+<div><center><h2>Your Pet - Our Passion</h2></center></div>
+<br/><br/>
+<p>Lorem ipsum dolor sit amet consectetur. <br/>
+Vitae in bibendum posuere nec sed. Semper urna sed leo fringilla. <br/>
+Pellentesque eget faucibus tristique aliquam pharetra. Consectetur augue sagittis sed aliquet.<br/>
+ Viverra pharetra purus rhoncus tellus at ultrices sapien. Vel aliquet ac sagittis nunc malesuada <br/>
+etiam vitae. Aliquet enim quam et quisque vel. Nunc suspendisse cras aliquet justo orci. <br/>
+Nibh vitae semper urna bibendum ultrices enim egestas nulla.
+</p>
+<br/><br/></br/><br/><br/></br/><br/></br/>
+
+
+</div>
+
+
+<div class="clear"></div>
+
+
+
+
+
+
+<div class="section-4"><br/><br/><br/><br/>><br/><br/><br/><br/><br/><br/>
+<div class="section-4-title">What We Offer</div>
+<p>Best Care For Your Best Friend</p>
+
+
+<br/><br/></br/>
+<div class="section-5">
+
+
+
+
+
+<div class="sec-5-1">
+<a href="#"><div style="overflow:hidden; border-radius:20px;"><img class="zoom" img src="./images/landing/sec-5.jpg" width=100% height=220px></div></a>
+<h3>Pet Shop</h3>
+<p>Lorem ipsum dolor sit amet consectetur.<br/>
+ Pellentesque rutrum vel hendrerit <br/>
+condimentum curabitur nunc risus neque.<br/> 
+Nibh egestas donec<br/>
+ ut volutpat cursus malesuada pretium.</p>
+
+<br />
+
+
+</div>
+
+<div class="sec-5-2">
+<a href="#"><div style="overflow:hidden; border-radius:20px;"><img class="zoom" img src="./images/landing/sec-5.jpg" width=100% height=220px></div></a>
+<h3>Pet Day-Care</h3>
+<p>Lorem ipsum dolor sit amet consectetur.<br/>
+ Pellentesque rutrum vel hendrerit <br/>
+condimentum curabitur nunc risus neque.<br/> 
+Nibh egestas donec<br/>
+ ut volutpat cursus malesuada pretium.</p>
+
+<br />
+</div>
+
+<div class="sec-5-3">
+<a href="#"><div style="overflow:hidden; border-radius:20px;"><img class="zoom" img src="./images/landing/sec-5.jpg" width=100% height=220px></div></a>
+<h3>Pet Boarding</h3>
+<p>Lorem ipsum dolor sit amet consectetur.<br/>
+ Pellentesque rutrum vel hendrerit <br/>
+condimentum curabitur nunc risus neque.<br/> 
+Nibh egestas donec<br/>
+ ut volutpat cursus malesuada pretium.</p>
+
+<br />
+</div>
+
+<div class="sec-5-4">
+<a href="#"><div style="overflow:hidden; border-radius:20px;"><img class="zoom" img src="./images/landing/sec-5.jpg" width=100% height=220px></div></a>
+<h3>Pet Meet-Up</h3>
+<p>Lorem ipsum dolor sit amet consectetur.<br/>
+ Pellentesque rutrum vel hendrerit <br/>
+condimentum curabitur nunc risus neque.<br/> 
+Nibh egestas donec<br/>
+ ut volutpat cursus malesuada pretium.</p>
+
+<br />
+</div>
+<br /><br />
+
+
+
+
+
+
+
+</div>
+
+
+</div>
+
+
+
+<div class="clear"></div>
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<div class="section-6">
+
+
+</div>
+
+
+
+</body>
+</html>
