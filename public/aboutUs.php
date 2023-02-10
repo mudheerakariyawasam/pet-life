09b09398838d4dbe642bd75b1c3670dcf82e893b
SandunUCSC <2020is021@stu.ucsc.cmb.ac.lk>
2023-02-10 12:40:19 +0530

    landing page done

---
+++ public/aboutUs.php
@@ -0,0 +1,185 @@
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="/pet-life/public/style/aboutUs.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+</head>
+<body>
+
+<div class="nav-bar">
+<div class="nav-left"><img class="logo" src="/pet-life/public/img/logo_transparent black.png"></div>
+<div class="nav-right">
+<ul class="navbar-ul">
+<li><a href="./index.php">Home</a></li>
+<li><a href="#">About Us</a></li>
+<li><a href="#">VIP Programs</a></li>
+<li><a href="./services.php">Services</a></li>
+<li><a href="./Store/store.php">Pet Store</a></li>
+<li><a href="./contactUs">Contact Us</a></li>
+<li><a href="#">Make an appointment</a></li>
+<li><a href="#">Sign Up</a></li>
+<li><a href="/pet-life/Auth/login.php">Login</a></li>
+</ul>
+
+
+
+</div>
+</div>
+<div class="clear"></div>
+
+
+<div class="home-section">
+
+
+
+</div>
+
+
+<div class="abt-us">
+<div class="abt-title">
+    <p>About Us</p>
+    <hr/>
+    <br/>
+</div>
+<div class="abt-content"><p>
+The Pet Life animal hospital is driven by a passion for helping pets and giving their people<br>
+the support they deserve. We know that your pet is precious to your family,<br/>
+and when they're happy, you're happy.<br/>
+For more insight into who we are and what we do, take a moment to learn about our values and</br>
+meet our amazing veterinary teams in Pet Life.
+
+</p>
+<br/>
+</div>
+</div>
+
+<div class="mission">
+<br/>
+    <div class="mission-sec">
+    
+        <div class="mission-sec-1">
+            <p>OUR</br> MISSION</p>
+        </div>
+        <div class="mission-sec-2">
+            <p>Our purpose is to enrich the lives of pets and their families through personalized</br>partnerships</p>
+        </div>
+    </div>
+
+
+<br/>
+</div>
+
+<div class="values">
+<div class="value-title"><h2>Our Core Values</h2></div>
+<br/>
+
+
+<div class="value-row">
+    <div class="value-row-1">
+        <h2>PASSION</h2>
+        <p>
+        We love what we do and couldn't imagine a world without our</br>
+four-legged companions. We embrace the unique gift that is the </br>
+human-animal bond, and are committed to enriching the lives of</br>
+our patients and their families. Passion  is our driving force.
+
+        </p>
+    </div>
+<div class="value-row-2">
+<h2>TRUST</h2>
+        <p>
+        We love what we do and couldn't imagine a world without our</br>
+four-legged companions. We embrace the unique gift that is the </br>
+human-animal bond, and are committed to enriching the lives of</br>
+our patients and their families. Passion  is our driving force.
+
+        </p>
+</div>
+
+
+
+</div>
+
+<div class="value-row">
+    <div class="value-row-1">
+    <h2>COMMUNICATION</h2>
+        <p>
+        We love what we do and couldn't imagine a world without our</br>
+four-legged companions. We embrace the unique gift that is the </br>
+human-animal bond, and are committed to enriching the lives of</br>
+our patients and their families. Passion  is our driving force.
+
+        </p>
+    </div>
+<div class="value-row-2">
+<h2>TEAM WORK</h2>
+        <p>
+        We love what we do and couldn't imagine a world without our</br>
+four-legged companions. We embrace the unique gift that is the </br>
+human-animal bond, and are committed to enriching the lives of</br>
+our patients and their families. Passion  is our driving force.
+
+        </p>
+</div>
+</div>
+
+<br/><br/>
+</div>
+
+<div class="footer">
+    <br/><br/>
+<div class="about-us">
+    <div class="about">
+<p class="title">About Us</p>
+<hr/>
+<p>Lorem ipsum dolor sit consectetur.<br/>
+Vitae in bibendum posuere nec sed. urna sed.<br/>
+Pellentesque eget faucibus tri aliquam pharetra.<br/>
+ Viverra pharetra purus rhoncus tellultrices sapien.<br/>
+etiam vitae. Aliquet enim quam et vel.<br/>
+Nibh vitae semper ua bibendu.
+</p>
+<br/>
+<p>Read More.........</p>
+
+
+
+    </div>
+    <div class="ulinks">
+    <p class="title">useful Links</p>
+<hr/>
+<ul>
+<li>Home</li>
+<hr>
+<li>About Us</li>
+<hr>
+<li>VIP Programs</li>
+<hr>
+<li>Services</li>
+<hr>
+<li>Pet Store</li>
+<hr>
+<li>Contact Us</li><br/>
+
+
+</ul>
+
+    </div>
+    <div class="gitouch">
+    <p class="title">Get In Touch</p>
+<hr/>
+<ul>
+<li><i class="fa-solid fa-location-dot"></i><span>Galle Rd, Colombo</span></li><br/>
+<li><i class="fa-solid fa-phone-volume"></i><span>94 7178992 902/ +94 71178992 901</span></li><br/>
+<li><i class="fa-solid fa-envelope"></i><span>petlife.lk</span></li><br/>
+
+
+
+</ul>
+
+    </div>
+</div>
+
+</div>
+</body>
+</html>
---
+++ public/services.php
@@ -0,0 +1,158 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+<link rel="stylesheet" type="text/css" href="/pet-life/public/style/services.css">
+</head>
+<body>
+
+<div class="nav-bar">
+<div class="nav-left"><img class="logo" src="/pet-life/public/img/logo_transparent black.png"></div>
+<div class="nav-right">
+<ul class="navbar-ul">
+<li><a href="./index.php">Home</a></li>
+<li><a href="./aboutUs.php">About Us</a></li>
+<li><a href="#">VIP Programs</a></li>
+<li><a href="#">Services</a></li>
+<li><a href="./Store/store.php">Pet Store</a></li>
+<li><a href="./contactUs.php">Contact Us</a></li>
+<li><a href="#">Make an appointment</a></li>
+<li><a href="#">Sign Up</a></li>
+<li><a href="/pet-life/Auth/login.php">Login</a></li>
+</ul>
+
+
+
+</div>
+</div>
+<div class="clear"></div>
+
+
+<div class="home-section"></div>
+<div class="home-content">
+<br/>
+<center><p class="home-content-title">Services We Offer</p></center>
+
+<p class="home-content-title-p" style="text-align:center; font-size:15px;">Lorem ipsum dolor sit amet consectetur. <br/>
+Vitae in bibendum posuere nec sed. Semper urna sed leo fringilla. <br/>
+Pellentesque eget faucibus tristique aliquam pharetra.<br/>
+Viverra pharetra purus rhoncus tellus at ultrices <br/>
+felis.
+</p>
+<div class="features">
+<div class="fea-1">
+<img src="/pet-life/public/img/star-1.png">
+<br>
+<p style="font-size:16px;padding:10px; font-weight:bold; margin-left:40px;"">Dental Services</p>
+<p style="padding:10px; font-size:16px;">Vitae in bibendum<br/>
+Pellentesque eget faucibu<br/>
+Viverra pharetra purus<br/> 
+Nibh vitae semp<br/>
+Vitae in bibendum.<br/>
+Pellentesqu nbhh<br/>
+Viverra pharetra purus<br/> 
+Nibh vitae semper<br/>
+Vitae in bibendum sed.<br/>
+Pellentesque <br/>
+Viverra pharet<br/> 
+Nibh vitae semper<br/></p>
+</div>
+<div class="fea-2"><img src="/pet-life/public/img/star-2.png">
+<br>
+<p style="font-size:16px;padding:10px; font-weight:bold; margin-left:40px;">Pet Boarding</p>
+<p style="padding:10px; font-size:16px;">Vitae in bibendum<br/>
+Pellentesque eget faucibu<br/>
+Viverra pharetra purus<br/> 
+Nibh vitae semp<br/>
+Vitae in bibendum.<br/>
+Pellentesqu nbhh<br/>
+Viverra pharetra purus<br/> 
+Nibh vitae semper<br/>
+Vitae in bibendum sed.<br/>
+Pellentesque <br/>
+Viverra pharet<br/> 
+Nibh vitae semper<br/></p>
+</div>
+<div class="fea-3"><img src="/pet-life/public/img/star-3.png">
+<br>
+<p style="font-size:16px;padding:10px; font-weight:bold; margin-left:40px;"">Pet Meet-Ups</p>
+<p style="padding:10px; font-size:16px;">Vitae in bibendum<br/>
+Pellentesque eget faucibu<br/>
+Viverra pharetra purus<br/> 
+Nibh vitae semp<br/>
+Vitae in bibendum.<br/>
+Pellentesqu nbhh<br/>
+Viverra pharetra purus<br/> 
+Nibh vitae semper<br/>
+Vitae in bibendum sed.<br/>
+Pellentesque <br/>
+Viverra pharet<br/> 
+Nibh vitae semper<br/></p>
+</div>
+</div>
+
+</div>
+
+
+
+
+
+<div classs="clear"></div>
+<div class="footer">
+    <br/><br/>
+<div class="about-us">
+    <div class="about">
+<p class="title">About Us</p>
+<hr/>
+<p>Lorem ipsum dolor sit consectetur.<br/>
+Vitae in bibendum posuere nec sed. urna sed.<br/>
+Pellentesque eget faucibus tri aliquam pharetra.<br/>
+ Viverra pharetra purus rhoncus tellultrices sapien.<br/>
+etiam vitae. Aliquet enim quam et vel.<br/>
+Nibh vitae semper ua bibendu.
+</p>
+<br/>
+<p>Read More.........</p>
+
+
+
+    </div>
+    <div class="ulinks">
+    <p class="title">useful Links</p>
+<hr/>
+<ul>
+<li>Home</li>
+<hr>
+<li>About Us</li>
+<hr>
+<li>VIP Programs</li>
+<hr>
+<li>Services</li>
+<hr>
+<li>Pet Store</li>
+<hr>
+<li>Contact Us</li><br/>
+
+
+</ul>
+
+    </div>
+    <div class="gitouch">
+    <p class="title">Get In Touch</p>
+<hr/>
+<ul>
+<li><i class="fa-solid fa-location-dot"></i><span>Galle Rd, Colombo</span></li><br/>
+<li><i class="fa-solid fa-phone-volume"></i><span>94 7178992 902/ +94 71178992 901</span></li><br/>
+<li><i class="fa-solid fa-envelope"></i><span>petlife.lk</span></li><br/>
+
+
+
+</ul>
+
+    </div>
+</div>
+
+</div>
+
+
+</body>
+</html>
