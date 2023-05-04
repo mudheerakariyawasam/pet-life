9cb3ecf833c4fbbb5fb9aa29e82c38a096947c6c
senuri <senuridilshara@gmail.com>
2023-05-04 23:54:57 +0530

    Create client and pet detail view page

--- modules/veterinarian/controllers/viewcustomer.php
+++ modules/veterinarian/controllers/viewcustomer.php
@@ -1,10 +1,56 @@
 <?php
-   include("../../../db/dbconnection.php");
-   session_start();
-   if (!isset($_SESSION["login_user"])) {
-       header("location:login.php");
-       exit;
-   }
+include($_SERVER['DOCUMENT_ROOT'] . '/pet-life/db/dbconnection.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/pet-life/modules/veterinarian/permission.php');
+
+//Execute a query to retrieve data from the database
+$sql = "SELECT owner_id,owner_fname,owner_lname,owner_email,owner_contactno,owner_address,owner_nic FROM pet_owner";
+
+$view_selected_client = mysqli_query($conn, $sql);
+
+//Process the retrieved data
+if ($view_selected_client->num_rows > 0) {
+    // Fetch the first row of data
+    $row = $view_selected_client->fetch_assoc();
+
+    // Retrieve the specific column values
+    $id = $row['owner_id'];
+    $fname = $row['owner_fname'];
+    $lname = $row['owner_lname'];
+    $fullName = $fname . " " . $lname;
+    $email = $row['owner_email'];
+    $tpn = $row['owner_contactno'];
+    $address = $row['owner_address'];
+    $nic = $row['owner_nic'];
+
+
+    // Retrieve pet details based on owner ID
+    $sqlPet = "SELECT * FROM pet WHERE owner_id = '$id'";
+    $resultPet = mysqli_query($conn, $sqlPet);
+
+
+
+    // Process the retrieved pet data
+    if ($resultPet->num_rows > 0) {
+        // Loop through each row of pet data
+        while ($rowPet = mysqli_fetch_assoc($resultPet)) {
+            // Retrieve the specific column values
+            $petID = $rowPet['pet_id'];
+            $petName = $rowPet['pet_name'];
+            $petGender = $rowPet['pet_gender'];
+            $petAge = $rowPet['pet_dob'];
+            $petWeight = $rowPet['pet_weight'];
+            $petType = $rowPet['pet_type'];
+            $petBreed = $rowPet['pet_breed'];
+        }
+    } else {
+        echo "No pets found for this owner.";
+    }
+} else {
+    echo "No data found.";
+    $conn->close();
+    exit();
+}
+?>
 ?>
 
 <!DOCTYPE html>
@@ -22,22 +68,25 @@
 
 <body>
     <div class="sidebar">
-<div class="user-img"><center><img src="../images/logo_transparent black.png"></center></div>
+        <div class="user-img">
+            <center><img src="../images/logo_transparent black.png"></center>
+        </div>
         <ul>
-        <li>
+            <li>
                 <a href="dashboard.php"><i class="fa fa-tachometer"></i><span>Dashboard</span></a>
             </li>
-           
+
             <li>
                 <a href="showclients.php" class="active"><i class="fa fa-user"></i></i><span>Clients</span></a>
             </li>
             <li>
-                <a href="treatment_history.php" class="active"><i class="fa-solid fa-calendar-plus"></i><span>Treatment History</span></a></a>
+                <a href="treatment_history.php"><i class="fa-solid fa-calendar-plus"></i><span>Treatment
+                        History</span></a></a>
             </li>
             <li>
                 <a href="leaverequest.php"><i class="fa-solid fa-file"></i><span>Leave Request</span></a></a>
             </li>
-         
+
             <li>
                 <a href="updateprofile.php"><i class="fa-solid fa-circle-user"></i><span>My Profile</span></a>
             </li>
@@ -57,7 +106,9 @@
                 </div>
                 <div class="hello">
                     <font class="header-font-1">Welcome </font> &nbsp
-                    <font class="header-font-2"><?php echo $_SESSION['user_name']; ?></font>
+                    <font class="header-font-2">
+                        <?php echo $_SESSION['user_name']; ?>
+                    </font>
                 </div>
             </div>
 
@@ -71,131 +122,149 @@
                     </li>
                     <li>
                         <a href="#">
-                        <i class="fa-solid fa-message"></i>
+                            <i class="fa-solid fa-message"></i>
                         </a>
                     </li>
-                    
+
                 </ul>
             </div>
         </div>
         <div class="container">
-            <br/><br/><br/>
-<div class="cont">
-<div class="cont1">
-<center><img src="../images/buttler.png"><br/> Mr.Sachintha Perera</br>sachintha@gmail.com</center>
-<div class="appoint">
-<center>Appointments</center>
-<div class="appointment">
-   
-    <div class="appoint1"><p>5<br/>Past</p></div>
-    <div class="appoint2"><p>2<br/>Upcoming</p></div>
-</div>
-</div>
-<div>
+            <br /><br /><br />
+            <div class="cont">
+                <div class="cont1">
+                    <center><img src="../images/buttler.png"><br />
+                        <?php echo $fullName; ?></br>
+                        <?php echo $email; ?>
+                    </center>
+                    <div class="appoint">
+                        <center>Appointments</center>
+                        <div class="appointment">
 
-<p>Pet Owner's ID</p>
-<p>O002</p>
-<hr/>
-<br/>
-<p>Phone Number</p>
-<p>0770 377 558</p>
-<hr/>
-</br>
-<p>National ID Number</p>
-<p>9916020015V</p>
-<hr/>
-<br/>
-<p>Address</p>
-<p>No23, Temple Road, Horana</p>
-<hr/>
-<br/>
-<p>City</p>
+                            <div class="appoint1">
+                                <p>5<br />Past</p>
+                            </div>
+                            <div class="appoint2">
+                                <p>2<br />Upcoming</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+
+                        <p>Pet Owner's ID</p>
+                        <p>
+                            <?php echo $id; ?>
+                        </p>
+                        <hr />
+                        <br />
+                        <p>Phone Number</p>
+                        <p>
+                            <?php echo $tpn; ?>
+                        </p>
+                        <hr />
+                        </br>
+                        <p>National ID Number</p>
+                        <p>
+                            <?php echo $nic; ?>
+                        </p>
+                        <hr />
+                        <br />
+                        <p>Address</p>
+                        <p>
+                            <?php echo $address; ?>
+                        </p>
+                        <hr />
+                        <br />
+                        <!-- <p>City</p>
 <p>Horana</p>
 <hr/>
 <br/>
 <p>Number Of Pets</p>
 <p>2</p>
-<hr/>
-</div>
-</div>
+<hr/> -->
+                    </div>
+                </div>
 
 
 
-<div class="cont2">
+                <div class="cont2">
 
-<div class="top-btn-set" style="display:flex; width:96%; margin-left:2%">
-    <div class="active-pet"><button style="background-color: #C38D9E;color: black;">Chester</button></div> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <div class="inactive-pet"style="width:33.33%;"><button>Dora</button></div>
-    
-</div>
-<br/>
-<!-- <hr/>
+                    <div class="top-btn-set" style="display:flex; width:96%; margin-left:2%">
+                        <div class="active-pet"><button style="background-color: #C38D9E;color: black;">Chester</button>
+                        </div> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <div class="inactive-pet" style="width:33.33%;"><button>Dora</button></div>
+
+                    </div>
+                    <br />
+                    <!-- <hr/>
 
 <br/> -->
 
-<div class="pet-name"><p>Pet Name : CHESTER</p></div>
+                    <div class="pet-name">
+                        <p>Pet Name : <?php echo $petName; ?></p>
+                    </div>
 
-<div class="dog-img"><img src="../images/dog.png" width=40%></div>
-
-
-<div class="pet-details">
-<div class="pet-details-list-1">
-<p>Pet ID</p>
-<p>P002</p>
-<hr/>
-<br/>
-
-<p>Requested Date</p>
-<p>23/12/2022</p>
-<hr/>
-<br/>
-
-<p>Age</p>
-<p>3 years</p>
-<hr/>
-<br/>
-
-<p>Weight</p>
-<p>7kg</p>
-<hr/>
-<br/>
-
-<p>Gender</p>
-<p>Male</p>
-<hr/>
-<br/>
-
-</div>
-<div class="pet-details-list-2">
-<p>Vacccines</p>
-<p>distemper</p>
-<hr/>
-<br/>
-
-<p>Breed</p>
-<p>Golden Retriever</p>
-<hr/>
-<br/>
-<br/>
-<div class="btn-bottom-set">
-    <button><a href="treatment_history.php">View Previous Treatments</a></button>
-    <br/><br/>
-    <a href="add_treatment_.php"><button>Add New Treatment</button></a>
-</div>
-
-</div>
-
-</div>
+                    <div class="dog-img"><img src="../images/dog.png" width=40%></div>
 
 
-</div>
-</div>
+                    <div class="pet-details">
+                        <div class="pet-details-list-1">
+                            <p>Pet ID</p>
+                            <p><?php echo $petID; ?></p>
+                            <hr />
+                            <br />
 
-       
+                            <!-- <p>Requested Date</p>
+                            <p>23/12/2022</p>
+                            <hr />
+                            <br /> -->
 
-            
-    </div>
-    <script src="script.js"></script>
+                            <p>Date of Birth</p>
+                            <p><?php echo $petAge; ?></p>
+                            <hr />
+                            <br />
+
+                            <p>Weight</p>
+                            <p><?php echo $petWeight; ?></p>
+                            <hr />
+                            <br />
+
+                            <p>Gender</p>
+                            <p><?php echo $petGender; ?></p>
+                            <hr />
+                            <br />
+
+                        </div>
+                        <div class="pet-details-list-2">
+                            <p>Pet Type</p>
+                            <p><?php echo $petType; ?></p>
+                            <hr />
+                            <br />
+
+                            <p>Breed</p>
+                            <p><?php echo $petBreed; ?></p>
+                            <hr />
+                            <br />
+                            <br />
+                            <div class="btn-bottom-set">
+                                <button><a href="treatment_history.php">View Previous Treatments</a></button>
+                                <br /><br />
+                                <a href="add_treatment_.php"><button>Add New Treatment</button></a>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                </div>
+            </div>
+
+
+
+
+        </div>
+        <script src="script.js"></script>
 </body>
 
 </html>
