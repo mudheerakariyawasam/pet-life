f99726a107984a233aa67b7f07b3759bc7acd8eb
senuri <senuridilshara@gmail.com>
2023-05-13 15:15:54 +0530

    error in add treatment

--- modules/veterinarian/controllers/leaverequest.php
+++ modules/veterinarian/controllers/leaverequest.php
@@ -94,7 +94,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/leaverequest.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-    <title>Leave Request</title>
+    <title></title>
 </head>
 <body>
 
