498f265ef5fbe5303e015cd1f6dc3e24697f413a
Mudheera Kariyawasam <2020is055@stu.ucsc.cmb.ac.lk>
2023-05-15 14:25:52 +0530

    test

--- modules/pet store/addbatch.php
+++ modules/pet store/addbatch.php
@@ -70,6 +70,8 @@ include($_SERVER['DOCUMENT_ROOT'] . '/pet-life/db/dbconnection.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/add.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.6/dist/sweetalert2.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.6/dist/sweetalert2.min.js"></script>    
     <title>Pet Life</title>
 </head>
 <body>
